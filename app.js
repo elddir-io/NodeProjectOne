bf286a97398bb0cdd204fa866b7bcf198ce05323
elddir-io <robert@elddir.io>
2021-08-24 10:19:12 -0400

    serving HTML pages #70, add 404 page and route

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
+const path = require('path');
 const express = require ('express');
-require('dotenv').config();
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({extended: true})); // deprication of body-parser, so using built-in express body-parser
+app.use(express.urlencoded({extended: false})); // deprication of body-parser, so using built-in express body-parser
 
-app.use('/admin', adminRoutes);
+app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // app.use((req,res,next) => {});
 // app.send((req,res) => {});
-
-
-
-
-
-
-
-
-
 
 
 // app.get('/', (req,res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,11 @@
+const path = require('path'); // node core module
+
 const express = require('express');
 
 const router = express.Router();
 
-module.exports = router;
-
-router.get('/shop', (req,res,next) => {
-    
-    // res.sendFile(__dirname + "../shop.html");
+router.get('/', (req,res,next) => {    
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')); // up one level to root folder, into views folder, and serves the shop file
 });
 
- module.exports = router;
+module.exports = router;
